749f8314d19192907a23e02975815cdbaea5c5f8
Rais Ilham <rais@hidata.id>
2024-10-15 15:53:21 +0700

    Add logo Sevima InstaApp, Initial design for login and register done

---
+++ README.md
@@ -0,0 +1,12 @@
+# InstaApp
+
+## Tech Stack
+
+-   Php
+-   Laravel
+-   Mysql
+-   Vue.js
+-   Tailwindcss
+-   Inertia.js
+
+## Demo
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,22 +1,30 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+        class="flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0"
     >
         <div>
-            <Link href="/">
+            <!-- <Link href="/">
                 <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
+            </Link> -->
         </div>
 
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
-        >
-            <slot />
+        <div class="flex w-full justify-center items-center">
+            <!-- First column -->
+            <div class="">
+                <!-- load image asset -->
+                <img src="../../../public/images/instagram.png" alt="Logo" />
+            </div>
+            <!-- Second column -->
+            <div class="flex-grow max-w-sm border p-10 pt-20 pb-20">
+                <img src="../../../public/images/Sevima-logo.png" alt="Logo" />
+
+                <slot />
+            </div>
         </div>
     </div>
 </template>
